d91d4715d13033d2e4b9d37f41c6c59cebacecd5
jujuju88 <132356175+jujuju88@users.noreply.github.com>
2023-05-10 14:30:47 +0100

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,17 @@
+#Mad Libs Generator
+
+print("Input your words: ")
+occupation = input("Occupation(a job): ")
+noun1 = input("Noun:")
+adjective1 = input("Adjective: ")
+noun2 = input("Noun: ")
+verb1 = input("Verb: ")
+adjective2 = input("Adjective: ")
+noun3 = input("Noun: ")
+verb2 = input("Verb: ")
+noun4 = input("Noun: ")
+verb3 = input("Verb: ")
+
+print(f"Today a {occupation} named {noun4} came to our school to talk to us about her job. She said the most important skill you need to know to do her job is to be able to {verb2} around a {adjective1} {noun3}. She said it was easy for her to learn her job because she loved to {verb1} when she was my age--and that helps a lot! If you're considering her profession, I hope you can be near a {adjective2} {noun1}. That's very important! If you get too distracted in that situation you won't be able to {verb3} next to a {noun2}!")
+
+
